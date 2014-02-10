ad455a049154a636349ea13e2ca481e5fb85ad4b
Billy Lynch <wlynch92@gmail.com>
2014-02-10 16:21:07 -0500

    2/14/14

---
+++ rec3/Square.java
@@ -0,0 +1,7 @@
+public class Square {
+	public static void main(String args[]) {
+		System.out.print("Input a number:")
+		double x = IO.readDouble();
+		IO.outputDoubleAnswer(x*x);
+	}
+}
--- rec3/TestProgram.java
+++ rec3/TestProgram.java
@@ -1,9 +1,11 @@
-Public class TestProgram{
-Public void main(string[] args){
-system. Out. Println("x=" + x;
-x = 5
-y = 3
-z = x + y
-System.out.println(z);
+public class TestProgram {
+	public static void main(String[] args)
+	{
+		int x, y, z;
+		x = 5;
+		System.out.print("x=" + x);
+		y = 3;
+		z = x + y;
+		System.out.println(z);
+	}
 }
-
